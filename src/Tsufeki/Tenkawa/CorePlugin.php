1250ab4c50e921edeb8eac62a92ece4169ed7351
tsufeki <tsufeki@ymail.com>
2017-12-29 15:01:25 +0100

    Put indexer into DI.

--- src/Tsufeki/Tenkawa/CorePlugin.php
+++ src/Tsufeki/Tenkawa/CorePlugin.php
@@ -17,8 +17,19 @@ use Tsufeki\Tenkawa\Diagnostics\ParserDiagnosticsProvider;
 use Tsufeki\Tenkawa\Diagnostics\PhplDiagnosticsProvider;
 use Tsufeki\Tenkawa\Document\DocumentStore;
 use Tsufeki\Tenkawa\Event\Document\OnChange;
+use Tsufeki\Tenkawa\Event\Document\OnClose;
 use Tsufeki\Tenkawa\Event\Document\OnOpen;
+use Tsufeki\Tenkawa\Event\Document\OnProjectClose;
+use Tsufeki\Tenkawa\Event\Document\OnProjectOpen;
 use Tsufeki\Tenkawa\Event\OnStart;
+use Tsufeki\Tenkawa\Index\Indexer;
+use Tsufeki\Tenkawa\Index\IndexStorageFactory;
+use Tsufeki\Tenkawa\Index\LocalCacheIndexStorageFactory;
+use Tsufeki\Tenkawa\Io\Directories;
+use Tsufeki\Tenkawa\Io\FileReader;
+use Tsufeki\Tenkawa\Io\FileSearch;
+use Tsufeki\Tenkawa\Io\LocalFileReader;
+use Tsufeki\Tenkawa\Io\LocalFileSearch;
 use Tsufeki\Tenkawa\Mapper\UriMapper;
 use Tsufeki\Tenkawa\Parser\Parser;
 use Tsufeki\Tenkawa\Parser\PhpParserAdapter;
@@ -37,6 +48,10 @@ class CorePlugin extends Plugin
         $container->setClass(MethodRegistry::class, SimpleMethodRegistry::class);
         $container->setCallable(MappedJsonRpc::class, [MappedJsonRpc::class, 'create']);
 
+        $container->setClass(Directories::class);
+        $container->setClass(FileReader::class, LocalFileReader::class);
+        $container->setClass(FileSearch::class, LocalFileSearch::class);
+
         $container->setClass(ReactProcessRunner::class);
         $container->setClass(ProcessRunner::class, ThrottledProcessRunner::class, false, [
             ReactProcessRunner::class,
@@ -54,6 +69,15 @@ class CorePlugin extends Plugin
 
         $container->setClass(Parser::class, PhpParserAdapter::class);
         $container->setClass(DiagnosticsProvider::class, ParserDiagnosticsProvider::class, true);
+
+        $container->setClass(IndexStorageFactory::class, LocalCacheIndexStorageFactory::class);
+        $container->setClass(Indexer::class);
+        $container->setAlias(OnStart::class, Indexer::class, true);
+        $container->setAlias(OnOpen::class, Indexer::class, true);
+        $container->setAlias(OnChange::class, Indexer::class, true);
+        $container->setAlias(OnClose::class, Indexer::class, true);
+        $container->setAlias(OnProjectOpen::class, Indexer::class, true);
+        $container->setAlias(OnProjectClose::class, Indexer::class, true);
     }
 
     /**
