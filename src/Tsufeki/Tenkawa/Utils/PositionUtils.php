de5c9b2e818ad51b9fe21c523e07abe2130e9f68
tsufeki <tsufeki@ymail.com>
2017-12-31 23:20:00 +0100

    Add PositionUtils::offsetFromPosition().

--- src/Tsufeki/Tenkawa/Utils/PositionUtils.php
+++ src/Tsufeki/Tenkawa/Utils/PositionUtils.php
@@ -8,31 +8,70 @@ use Tsufeki\Tenkawa\Protocol\Common\Range;
 
 class PositionUtils
 {
+    /**
+     * Return line beginnings offsets.
+     *
+     * @return int[]
+     */
+    private static function getLineOffsets(Document $document): array
+    {
+        if (null !== $lineOffsets = $document->get('line_offsets')) {
+            return $lineOffsets;
+        }
+
+        $text = $document->getText();
+        $lineOffsets = [];
+        $offset = 0;
+
+        while (true) {
+            $lineOffsets[] = $offset;
+            $offset = strpos($text, "\n", $offset);
+            if ($offset === false) {
+                break;
+            }
+            $offset++; // newline character
+        }
+
+        $lineOffsets[] = strlen($text);
+        $document->set('line_offsets', $lineOffsets);
+
+        return $lineOffsets;
+    }
+
     public static function positionFromOffset(int $offset, Document $document): Position
     {
         $text = $document->getText();
         $offset = max(0, min($offset, strlen($text) - 1));
 
-        $line = -1;
-        $currentOffset = 0;
-        $lastOffset = 0;
-
-        while ($currentOffset <= $offset) {
-            $lastOffset = $currentOffset;
-            $currentOffset = strpos($text, "\n", $currentOffset);
+        $lineOffsets = self::getLineOffsets($document);
+        $line = 0;
+        while ($lineOffsets[$line + 1] < $offset) {
             $line++;
-            if ($currentOffset === false) {
-                break;
-            }
-            $currentOffset++; // newline character
         }
 
-        $column = max(0, $offset - $lastOffset);
+        $column = max(0, $offset - $lineOffsets[$line]);
         // Protocol defines column number as UTF-16 code unit count
-        $textBefore = substr($text, $lastOffset, $column);
+        $textBefore = substr($text, $lineOffsets[$line], $column);
         $columnUtf16 = strlen(mb_convert_encoding($textBefore, 'UTF-16LE', 'UTF-8')) >> 1;
 
         return new Position($line, $columnUtf16);
+    }
+
+    public static function offsetFromPosition(Position $position, Document $document): int
+    {
+        $text = $document->getText();
+        $lineOffsets = self::getLineOffsets($document);
+        $line = max(0, min($position->line, count($lineOffsets) - 2));
+
+        $offset = $lineOffsets[$line];
+        // Protocol defines column number as UTF-16 code unit count
+        $lineText = substr($text, $offset, $lineOffsets[$line + 1] - $offset);
+        $lineTextUtf16 = mb_convert_encoding($lineText, 'UTF-16LE', 'UTF-8');
+        $column = max(0, min($position->character, strlen($lineTextUtf16) >> 1));
+        $textBeforeUtf16 = substr($lineTextUtf16, 0, $column << 1);
+        $offset += strlen(mb_convert_encoding($textBeforeUtf16, 'UTF-8', 'UTF-16LE'));
+
+        return $offset;
     }
 
     public static function rangeFromNodeAttrs(array $attributes, Document $document): Range
--- tests/Tsufeki/Tenkawa/Utils/PositionUtilsTest.php
+++ tests/Tsufeki/Tenkawa/Utils/PositionUtilsTest.php
@@ -18,7 +18,7 @@ class PositionUtilsTest extends TestCase
     /**
      * @dataProvider data_position_from_offset
      */
-    public function test_position_from_offset($expectedLine, $expectedColumn, $text, $offset)
+    public function test_position_from_offset($line, $column, $text, $offset)
     {
         $project = new Project(Uri::fromString('file:///'));
         $document = new Document(Uri::fromString('file:///foo'), 'php', $project);
@@ -26,8 +26,8 @@ class PositionUtilsTest extends TestCase
 
         $pos = PositionUtils::positionFromOffset($offset, $document);
 
-        $this->assertSame($expectedLine, $pos->line);
-        $this->assertSame($expectedColumn, $pos->character);
+        $this->assertSame($line, $pos->line);
+        $this->assertSame($column, $pos->character);
     }
 
     public function data_position_from_offset(): array
@@ -54,6 +54,48 @@ class PositionUtilsTest extends TestCase
             [1, 1, "\nqaz\n\nwsx\n", 2],
             [3, 2, "\nqaz\n\nwsx\n", 8],
             [3, 3, "\nqaz\n\nwsx\n", 9],
+            [3, 3, "\nqaz\n\nwsx\n", 20],
+        ];
+    }
+
+    /**
+     * @dataProvider data_offset_from_position
+     */
+    public function test_offset_from_position($line, $column, $text, $expectedOffset)
+    {
+        $project = new Project(Uri::fromString('file:///'));
+        $document = new Document(Uri::fromString('file:///foo'), 'php', $project);
+        $document->update($text);
+
+        $offset = PositionUtils::offsetFromPosition(new Position($line, $column), $document);
+
+        $this->assertSame($expectedOffset, $offset);
+    }
+
+    public function data_offset_from_position(): array
+    {
+        return [
+            [0, 0, '', 0],
+
+            [0, 0, 'qaz', 0],
+            [0, 1, 'qaz', 1],
+            [0, 2, 'qaz', 2],
+            [0, 1, "qaz\n", 1],
+            [0, 2, "qaz\n", 2],
+            [0, 3, "qaz\n", 3],
+
+            [0, 1, "qaz\nwsx", 1],
+            [0, 3, "qaz\nwsx", 3],
+            [1, 1, "qaz\nwsx", 5],
+            [1, 2, "qaz\nwsx", 6],
+            [1, 3, "qaz\na𐐀b", 9],
+
+            [0, 0, "\nqaz\n\nwsx\n", 0],
+            [1, 1, "\nqaz\n\nwsx\n", 2],
+            [3, 2, "\nqaz\n\nwsx\n", 8],
+            [3, 3, "\nqaz\n\nwsx\n", 9],
+            [3, 9, "\nqaz\n\nwsx\n", 10],
+            [9, 3, "\nqaz\n\nwsx\n", 10],
         ];
     }
 
