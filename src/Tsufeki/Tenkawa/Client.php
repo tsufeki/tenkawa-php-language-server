c928ed8631c198cb119386bfab3af9930c38a3ce
tsufeki <tsufeki@ymail.com>
2018-01-30 21:38:00 +0100

    Log outgoing requests and notifications.

--- src/Tsufeki/Tenkawa/Client.php
+++ src/Tsufeki/Tenkawa/Client.php
@@ -2,6 +2,7 @@
 
 namespace Tsufeki\Tenkawa;
 
+use Psr\Log\LoggerInterface;
 use Tsufeki\BlancheJsonRpc\MappedJsonRpc;
 use Tsufeki\Tenkawa\Protocol\LanguageClient;
 
@@ -12,18 +13,26 @@ class Client extends LanguageClient
      */
     private $rpc;
 
-    public function __construct(MappedJsonRpc $rpc)
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(MappedJsonRpc $rpc, LoggerInterface $logger)
     {
         $this->rpc = $rpc;
+        $this->logger = $logger;
     }
 
     public function publishDiagnostics(Uri $uri, array $diagnostics): \Generator
     {
+        $this->logger->debug('send: ' . __FUNCTION__ . " $uri");
         yield $this->rpc->notify('textDocument/publishDiagnostics', compact('uri', 'diagnostics'));
     }
 
     public function logMessage(int $type, string $message): \Generator
     {
+        $this->logger->debug('send: ' . __FUNCTION__ . " $message");
         yield $this->rpc->notify('window/logMessage', compact('type', 'message'));
     }
 }
