1f501bcb247d6b7ad9443b58d7428f468835480d
tsufeki <tsufeki@ymail.com>
2018-08-23 22:17:34 +0200

    Throttle concurrent file reads.

--- src/Tsufeki/Tenkawa/Server/Io/LocalFileReader.php
+++ src/Tsufeki/Tenkawa/Server/Io/LocalFileReader.php
@@ -5,25 +5,50 @@ namespace Tsufeki\Tenkawa\Server\Io;
 use Recoil\Recoil;
 use Tsufeki\Tenkawa\Server\Exception\IoException;
 use Tsufeki\Tenkawa\Server\Uri;
+use Tsufeki\Tenkawa\Server\Utils\Throttler;
 
 class LocalFileReader implements FileReader
 {
     private const MAX_SIZE = 1 * 1024 * 1024;
 
+    private const MAX_CONCURRENT = 30;
+
+    /**
+     * @var Throttler
+     */
+    private $throttler;
+
+    public function __construct()
+    {
+        $this->throttler = new Throttler(self::MAX_CONCURRENT);
+    }
+
     public function read(Uri $uri): \Generator
     {
-        $file = @fopen($uri->getFilesystemPath(), 'r');
-        if ($file === false) {
-            throw new IoException("Can't open file $uri");
-        }
+        $job = function () use ($uri): \Generator {
+            $file = false;
 
-        stream_set_blocking($file, false);
+            try {
+                $file = @fopen($uri->getFilesystemPath(), 'r');
+                if ($file === false) {
+                    throw new IoException("Can't open file $uri");
+                }
 
-        $content = yield Recoil::read($file, self::MAX_SIZE + 1, self::MAX_SIZE + 1);
-        if (strlen($content) > self::MAX_SIZE) {
-            throw new IoException("File size limit exceeded for $uri");
-        }
+                stream_set_blocking($file, false);
 
-        return $content;
+                $content = yield Recoil::read($file, self::MAX_SIZE + 1, self::MAX_SIZE + 1);
+                if (strlen($content) > self::MAX_SIZE) {
+                    throw new IoException("File size limit exceeded for $uri");
+                }
+            } finally {
+                if (is_resource($file)) {
+                    @fclose($file);
+                }
+            }
+
+            return $content;
+        };
+
+        return yield $this->throttler->run($job);
     }
 }
